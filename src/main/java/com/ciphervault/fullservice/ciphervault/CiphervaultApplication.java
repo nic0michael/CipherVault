7baa8a79103158a081e85128a53d1f0e573c7607
nic0michael <nicomichael2012@gmail.com>
2025-09-21 13:58:43 +0200

    New Files v3

---
+++ src/main/java/com/ciphervault/fullservice/ciphervault/CiphervaultApplication.java
@@ -0,0 +1,13 @@
+package com.ciphervault.fullservice.ciphervault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphervaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphervaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciphervault/fullservice/ciphervault/CiphervaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciphervault.fullservice.ciphervault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiphervaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
